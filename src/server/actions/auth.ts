e1637a398c93373785e0072514d568862d96946e
ayaosamaramadan <ayaramadan2011@gmail.com>
2025-04-14 07:42:23 +0200

    handle signup

--- src/server/actions/auth.ts
+++ src/server/actions/auth.ts
@@ -1,6 +1,6 @@
 "use server";
 import { db } from "@/lib/prisma";
-import { loginSchema } from "@/validations/auth";
+import { loginSchema, signupSchema } from "@/validations/auth";
 import bcrypt from "bcrypt";
 export const loginUser = async (credentials: Record<"email" | "password", string> | undefined) => {
    
@@ -54,5 +54,62 @@ export const loginUser = async (credentials: Record<"email" | "password", string
    
 
 
+
+}
+
+
+export const signNewUser = async (prevState:unknown,formDate:FormData)=>{
+    // console.log("formDate",formDate);
+    
+    const result = signupSchema().safeParse(
+        Object.fromEntries(formDate.entries())
+    );
+
+    if(!result.success) {
+        return { error: result.error.format(),
+            FormData,
+            status: 400 
+         };
+    }
+
+   try{
+ 
+    const user = await db.user.findUnique({
+        where: {
+            email: result.data.email,
+        },
+    });
+  
+    if (user) {
+        return {  formDate, error: "User already exists", status: 409 };
+      
+    }
+
+    const hashedPassword = await bcrypt.hash(result.data.password, 10);
+  
+    const newUser = await db.user.create({
+        data: {
+            name: result.data.name,
+            email: result.data.email,
+            password: hashedPassword,
+        },
+    });
+
+    return {
+        user: {
+            id: newUser.id,
+            name: newUser.name,
+            email: newUser.email,
+        },
+        message: "User created successfully",
+        status: 201,
+    };
+
+   }
+
+   catch (error) {
+        console.error("Error creating user:", error);
+        return { error: "User not found", status: 404 };
+    }   
 
 }
--- src/validations/auth.ts
+++ src/validations/auth.ts
@@ -9,6 +9,7 @@ export const loginSchema = () => {
     });
 };
 
+
 export const signupSchema = () => {
     return z.object({
         name: z.string().trim().min(1, "Name is required"),
@@ -16,5 +17,9 @@ export const signupSchema = () => {
         password: z.string()
             .min(8, "Password must be at least 8 characters long")
             .max(20, "Password must be at most 20 characters long"),
+        confirmPassword: z.string().min(8, "Confirm Password is required")
+    }).refine((data: { password: string; confirmPassword: string }) => data.password === data.confirmPassword, {
+        message: "Passwords don't match",
+        path: ["confirmPassword"],
     });
 };
